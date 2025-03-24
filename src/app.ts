c26d95d6ed18f5cf3133a27d9fe78c510f22f348
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-03-24 20:25:37 +0100

    welcome route and router added

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Application, Request, Response } from 'express';
 import dotenvFlow from 'dotenv-flow';
+import routes from './routes'
 
 // Load environment variables
 dotenvFlow.config();
 
 // Create Express application
 const app: Application = express();
+
+app.use('/api', routes);
 
 /**
  * 
---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Router, Request, Response } from 'express';
+
+const router: Router = Router();
+
+// Health check route
+router.get('/', (req: Request, res: Response) => {
+   res.status(200).send('Hello Travelers!');
+})
+
+export default router;
