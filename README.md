52addbaf55e06357885569b75232a09e56714a2d
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-03-24 20:45:09 +0100

    dynamic port and readme update

--- README.md
+++ README.md
@@ -2,14 +2,18 @@
 
 MonMongoDB Express Node TypeScript REST API for travel recommendations db
 
-## Project Setup
+## Project setup
 
 ```sh
 npm install
 ```
 
-### Start the Development
+### Start the development
 
 ```sh
 npm run dev
 ```
+
+### Get the .env file
+
+Get the .env file and put it in the root of the project
--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,8 @@ app.use('/api', routes);
  * 
  */
 export function startServer(): void {
-   app.listen(4000, () => {
-      console.log('Server is running on port: ' + 4000);
+   const PORT: number = parseInt(process.env.PORT as string) || 4000;
+   app.listen(PORT, () => {
+      console.log('Server is running on port: ' + PORT);
    })
 }
