d5290628dc4328768bb841f15be686bae866c70d
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-04-05 12:35:45 +0200

    dataSeeder place update

--- src/util/dataSeeder.ts
+++ src/util/dataSeeder.ts
@@ -1,7 +1,7 @@
 import { permissionModel } from "../models/permissionModel";
 import { roleModel } from "../models/roleModel";
 import { userModel } from "../models/userModel";
-import { placeModel } from "src/models/placeModel";
+import { placeModel } from "../models/placeModel";
 
 import { connect, disconnect } from "../repository/database";
 import bcrypt from "bcrypt";
@@ -45,7 +45,6 @@ export async function seedData(): Promise<void> {
       { name: "user:assignRoles", description: "Allows to assign roles to another user", },
       { name: "content:managePlace", description: "Allows to approve or decline a suggested place" }
    ];
-
    await permissionModel.insertMany(permissions);
 
    // Create roles
@@ -65,7 +64,6 @@ export async function seedData(): Promise<void> {
          permissions: []
       }
    ];
-
    await roleModel.insertMany(roles);
 
    // Create users
@@ -140,77 +138,73 @@ export async function seedData(): Promise<void> {
    await user4.save();
 
    // Create places
-   const place1 = new placeModel({
-      name: "Place 1",
-      description: "Description for Place 1",
-      images: ["image1.jpg", "image2.jpg"],
-      location: {
-         continent: "Europe",
-         country: "Hungary",
-         city: "Budapest",
-         street: "Andrassy ut",
-         streetNumber: "123"
+   const places = [
+      {
+         name: "Place 1",
+         description: "Description for Place 1",
+         images: ["image1.jpg", "image2.jpg"],
+         location: {
+            continent: "Europe",
+            country: "Hungary",
+            city: "Budapest",
+            street: "Andrassy ut",
+            streetNumber: "123"
+         },
+         upvotes: 16,
+         tags: ["tag1", "tag2"],
+         approved: true,
+         _createdBy: user1._id
       },
-      upvotes: 16,
-      tags: ["tag1", "tag2"],
-      approved: true,
-      _createdBy: user1._id
-   });
-   await place1.save();
-
-   const place2 = new placeModel({
-      name: "Place 2",
-      description: "Description for Place 2",
-      images: ["image3.jpg", "image4.jpg"],
-      location: {
-         continent: "North America",
-         country: "USA",
-         city: "Seattle",
-         street: "Pine Street",
-         streetNumber: "22"
+      {
+         name: "Place 2",
+         description: "Description for Place 2",
+         images: ["image3.jpg", "image4.jpg"],
+         location: {
+            continent: "North America",
+            country: "USA",
+            city: "Seattle",
+            street: "Pine Street",
+            streetNumber: "22"
+         },
+         upvotes: 0,
+         tags: ["tag1", "tag2"],
+         approved: true,
+         _createdBy: user1._id
       },
-      upvotes: 0,
-      tags: ["tag1", "tag2"],
-      approved: true,
-      _createdBy: user1._id
-   });
-   await place2.save();
-
-   const place3 = new placeModel({
-      name: "Place 3",
-      description: "Description for Place 3",
-      images: ["image5.jpg", "image6.jpg"],
-      location: {
-         continent: "Europe",
-         country: "England",
-         city: "London",
-         street: "Baker Street",
-         streetNumber: "1A"
+      {
+         name: "Place 3",
+         description: "Description for Place 3",
+         images: ["image5.jpg", "image6.jpg"],
+         location: {
+            continent: "Europe",
+            country: "England",
+            city: "London",
+            street: "Baker Street",
+            streetNumber: "1A"
+         },
+         upvotes: 5,
+         tags: ["tag1", "tag2"],
+         approved: true,
+         _createdBy: user4._id
       },
-      upvotes: 5,
-      tags: ["tag1", "tag2"],
-      approved: true,
-      _createdBy: user4._id
-   });
-   await place3.save();
-
-   const place4 = new placeModel({
-      name: "Place 4",
-      description: "Description for Place 4",
-      images: ["image7.jpg", "image8.jpg"],
-      location: {
-         continent: "Asia",
-         country: "Japan",
-         city: "Tokyo",
-         street: "Shibuya",
-         streetNumber: "45"
-      },
-      upvotes: 0,
-      tags: ["tag1", "tag2"],
-      approved: false,
-      _createdBy: user4._id
-   });
-   await place4.save();
+      {
+         name: "Place 4",
+         description: "Description for Place 4",
+         images: ["image7.jpg", "image8.jpg"],
+         location: {
+            continent: "Asia",
+            country: "Japan",
+            city: "Tokyo",
+            street: "Shibuya",
+            streetNumber: "45"
+         },
+         upvotes: 0,
+         tags: ["tag1", "tag2"],
+         approved: false,
+         _createdBy: user4._id
+      }
+   ];
+   await placeModel.insertMany(places);
 
    console.log("Data seeded successfully.");
 }
